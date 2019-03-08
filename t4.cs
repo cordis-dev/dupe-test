ae460045342519e5f9ae0bd167eb043f4d446ca3
cordis-dev <cordis.dev@gmail.com>
2019-03-08 09:25:53 +0200

    Create t4.cs

---
+++ t4.cs
@@ -0,0 +1,19 @@
+    public class t4  
+    {
+        public void M1()
+        {
+            dynamic context = null;
+            context.Request10.Form["IntProperty_00"] = "1";
+            context.Request10.Form["IntProperty_01"] = "2";
+            context.Request10.Form["IntProperty_02"] = "3";
+            context.Request10.Form["IntProperty_03"] = "4";
+            context.Request10.Form["IntProperty_04"] = "5";
+            context.Request10.Form["IntProperty_05"] = "6";
+            context.Request10.Form["IntProperty_06"] = "7";
+            context.Request10.Form["IntProperty_07"] = "8";
+            context.Request10.Form["IntProperty_08"] = "9";
+            context.Request10.Form["IntProperty_09"] = "10";
+            context.Request10.Form["IntProperty_10"] = "11";
+            context.Request10.Form["IntProperty_11"] = "12";
+        }
+     }
