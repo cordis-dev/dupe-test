7c5905fb6ca1784ce578dd9afb7fce930e8a6c99
cordis-dev <darius@adroiti.com>
2026-01-05 11:24:13 +0200

    Update t6.cs

--- t6.cs
+++ t6.cs
@@ -15,5 +15,15 @@
             context.Request.Form["IntProperty_09"] = "10";
             context.Request.Form["IntProperty_10"] = "11";
             context.Request.Form["IntProperty_11"] = "12";
+            context.Request.Form["IntProperty_12"] = "13";
+            context.Request.Form["IntProperty_13"] = "14";
+            context.Request.Form["IntProperty_14"] = "15";
+            context.Request.Form["IntProperty_15"] = "16";
+            context.Request.Form["IntProperty_16"] = "17";
+            context.Request.Form["IntProperty_17"] = "18";
+            context.Request.Form["IntProperty_18"] = "19";
+            context.Request.Form["IntProperty_19"] = "20";
+            context.Request.Form["IntProperty_20"] = "21";
+            context.Request.Form["IntProperty_21"] = "22";
         }
     }
