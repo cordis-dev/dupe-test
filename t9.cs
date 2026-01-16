89d3c05ddb7aaed4577cd691423c52a08d590942
cordis-dev <darius@adroiti.com>
2026-01-16 15:52:14 +0200

    Create t9.cs

---
+++ t9.cs
@@ -0,0 +1,29 @@
+    public class t9
+    {
+        public void M()
+        {
+            dynamic context = null;
+            context.Request.Form["IntProperty_00"] = "1";
+            context.Request.Form["IntProperty_01"] = "2";
+            context.Request.Form["IntProperty_02"] = "3";
+            context.Request.Form["IntProperty_03"] = "4";
+            context.Request.Form["IntProperty_04"] = "5";
+            context.Request.Form["IntProperty_05"] = "6";
+            context.Request.Form["IntProperty_06"] = "7";
+            context.Request.Form["IntProperty_07"] = "8";
+            context.Request.Form["IntProperty_08"] = "9";
+            context.Request.Form["IntProperty_09"] = "10";
+            context.Request.Form["IntProperty_10"] = "11";
+            context.Request.Form["IntProperty_11"] = "12";
+            context.Request.Form["IntProperty_12"] = "13";
+            context.Request.Form["IntProperty_13"] = "14";
+            context.Request.Form["IntProperty_14"] = "15";
+            context.Request.Form["IntProperty_15"] = "16";
+            context.Request.Form["IntProperty_16"] = "17";
+            context.Request.Form["IntProperty_17"] = "18";
+            context.Request.Form["IntProperty_18"] = "19";
+            context.Request.Form["IntProperty_19"] = "20";
+            context.Request.Form["IntProperty_20"] = "21";
+            context.Request.Form["IntProperty_21"] = "22";
+        }
+    }
