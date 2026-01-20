e2f7e90414fbbf42329b95f8c1861c8c34229aa5
cordis-dev <darius@adroiti.com>
2026-01-20 15:46:45 +0200

    Create t20.cs

---
+++ src/t20.cs
@@ -0,0 +1,29 @@
+    public class t20
+    {
+        public void M()
+        {
+            dynamic context = null;
+            context.Request.Form["v_00"] = "1";
+            context.Request.Form["v_01"] = "2";
+            context.Request.Form["v_02"] = "3";
+            context.Request.Form["v_03"] = "4";
+            context.Request.Form["v_04"] = "5";
+            context.Request.Form["v_05"] = "6";
+            context.Request.Form["v_06"] = "7";
+            context.Request.Form["v_07"] = "8";
+            context.Request.Form["v_08"] = "9";
+            context.Request.Form["v_09"] = "10";
+            context.Request.Form["v_10"] = "11";
+            context.Request.Form["v_11"] = "12";
+            context.Request.Form["v_12"] = "13";
+            context.Request.Form["v_13"] = "14";
+            context.Request.Form["v_14"] = "15";
+            context.Request.Form["v_15"] = "16";
+            context.Request.Form["v_16"] = "17";
+            context.Request.Form["v_17"] = "18";
+            context.Request.Form["v_18"] = "19";
+            context.Request.Form["v_19"] = "20";
+            context.Request.Form["v_20"] = "21";
+            context.Request.Form["v_21"] = "22";
+        }
+    }
