2a99a38a7a8005d6b9b6db5952dc781693f4c04a
cordis-dev <darius@adroiti.com>
2026-01-20 16:12:45 +0200

    Create t32.cs

---
+++ src/inner/t32.cs
@@ -0,0 +1,29 @@
+    public class t32
+    {
+        public void M()
+        {
+            dynamic context = null;
+            context.Request.Form["vv_00"] = "1";
+            context.Request.Form["vv_01"] = "2";
+            context.Request.Form["vv_02"] = "3";
+            context.Request.Form["vv_03"] = "4";
+            context.Request.Form["vv_04"] = "5";
+            context.Request.Form["vv_05"] = "6";
+            context.Request.Form["vv_06"] = "7";
+            context.Request.Form["vv_07"] = "8";
+            context.Request.Form["vv_08"] = "9";
+            context.Request.Form["vv_09"] = "10";
+            context.Request.Form["vv_10"] = "11";
+            context.Request.Form["vv_11"] = "12";
+            context.Request.Form["vv_12"] = "13";
+            context.Request.Form["vv_13"] = "14";
+            context.Request.Form["vv_14"] = "15";
+            context.Request.Form["vv_15"] = "16";
+            context.Request.Form["vv_16"] = "17";
+            context.Request.Form["vv_17"] = "18";
+            context.Request.Form["vv_18"] = "19";
+            context.Request.Form["vv_19"] = "20";
+            context.Request.Form["vv_20"] = "21";
+            context.Request.Form["vv_21"] = "22";
+        }
+    }
